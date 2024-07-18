6548c4589c399f405e44194f8c24081d2051b0d3
Rupalpy <121295967+Rupalpy@users.noreply.github.com>
2024-07-18 17:28:51 +0530

    Create majority-element.cpp

---
+++ majority-element.cpp
@@ -0,0 +1,63 @@
+//Moore’s Voting Algorithm:
+class Solution {
+public:
+    int majorityElement(vector<int>& v) {
+        int n = v.size();
+        int cnt = 0; // count
+        int el; // Element
+
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = v[i];
+            }
+            else if (el == v[i]) cnt++;
+            else cnt--;
+        }
+
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
+    }
+};
+//using sorting
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         return nums[n/2];}
+// };
+
+
+
+
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
